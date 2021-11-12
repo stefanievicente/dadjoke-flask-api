87805fbca427bc5cd983b48fdff20bf7f36ad882
Stefanie <stefanie.vicente@hotmail.com>
2021-11-12 15:56:11 +0100

    Adding route

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from errors import errors
 app = Flask(__name__)
 api = Api(app, errors=errors)
 
-api.add_resource(DadJoke, '/dadjoke')
+api.add_resource(DadJoke, '/', '/dadjoke')
 
 if __name__ == '__main__':
     app.run(debug=True)
