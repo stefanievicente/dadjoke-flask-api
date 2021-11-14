b4b64513413ebe33cc911a0cbed9eef193b0d944
Stefanie <stefanie.vicente@hotmail.com>
2021-11-14 13:50:53 +0100

    Allow remote CORS requests

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
 from flask import Flask
 from flask_restful import Api
+from flask_cors import CORS
 
 from joke import DadJoke
 from errors import errors
 
 app = Flask(__name__)
 api = Api(app, errors=errors)
+CORS(app)
 
-api.add_resource(DadJoke, '/', '/dadjoke')
+api.add_resource(DadJoke, '/dadjoke')
 
 if __name__ == '__main__':
     app.run(debug=True)
